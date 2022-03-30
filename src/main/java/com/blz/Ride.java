bcf35c338effffeb266c9a3e4ff74ad1e9baf9ff
AbhijitSKshirsagar <abhijitkshirsagar793@gmail.com>
2022-03-31 02:59:26 +0530

    added: fare for multiple rides

---
+++ src/main/java/com/blz/Ride.java
@@ -0,0 +1,13 @@
+package com.blz;
+
+public class Ride {
+
+	public double distance;
+	public int time;
+
+	Ride(double distance, int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+
+}
